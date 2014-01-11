06db2302eabd3fd394e727586aa122d5cda8c3bb
ajlopez <webmaster@ajlopez.com>
2014-01-11 20:36:38 -0300

    Has free variable

---
+++ test/hasfree.js
@@ -0,0 +1,35 @@
+
+var sl = require('..');
+
+exports['Variable has itself as free'] = function (test) {
+    test.ok(sl.parse('x').hasFree('x'));
+}
+
+exports['Variable has other variable as not free'] = function (test) {
+    test.equal(sl.parse('x').hasFree('y'), false);
+}
+
+exports['Apply has first variable as free'] = function (test) {
+    test.ok(sl.parse('xy').hasFree('x'));
+}
+
+exports['Apply has second variable as free'] = function (test) {
+    test.ok(sl.parse('xy').hasFree('y'));
+}
+
+exports['Apply has not found variable as not free'] = function (test) {
+    test.equal(sl.parse('xy').hasFree('z'), false);
+}
+
+exports['Lambda has argument as not free'] = function (test) {
+    test.equal(sl.parse('\\x.y').hasFree('x'), false);
+}
+
+exports['Lambda has not bound variable as free'] = function (test) {
+    test.ok(sl.parse('\\x.y').hasFree('y'));
+}
+
+exports['Lambda has not found variable as not free'] = function (test) {
+    test.equal(sl.parse('\\x.y').hasFree('z'), false);
+}
+
